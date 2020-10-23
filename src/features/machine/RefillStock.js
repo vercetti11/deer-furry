f1ea6ffa08a7fd361cab82a793154027efa8753c
Octavio <octavio@vercetti.vision>
2020-10-23 09:40:27 +0200

    Presentational component wired to parent

--- src/features/machine/RefillStock.js
+++ src/features/machine/RefillStock.js
@@ -1,27 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllProducts, refillStockOfBeverages } from "./machineSlice";
 import { Button, Pane, TextInputField, Pill } from "evergreen-ui";
 
-const Stock = ({ name, stock }) => {
+const Stock = ({ name, stock, index, sumbitToParent }) => {
+  const [ammountToRefill, setAmmountToRefill] = useState();
+  const [validationMessage, setValidationMessage] = useState();
+  const handleSumbit = e => {
+    e.preventDefault();
+    if (ammountToRefill) {
+      sumbitToParent();
+      return;
+    }
+    setValidationMessage("This field is required");
+  };
   return (
     <Pane>
       <Pill>{stock}</Pill>
-      <TextInputField
-        label={name}
-        description="Quantity to refill slot."
-        placeholder="Insert Number"
-        marginBottom={8}
-        type="number"
-        pattern="[0-9]*"
-      />
-      <Button>Refill</Button>
+      <form onSubmit={handleSumbit}>
+        <TextInputField
+          label={name}
+          onChange={e => setAmmountToRefill(e.target.value)}
+          isInvalid={false}
+          description="Quantity to refill slot."
+          placeholder="Insert Number"
+          marginBottom={8}
+          type="number"
+          pattern="[0-9]*"
+          validationMessage={validationMessage}
+        />
+        <Button>Refill</Button>
+      </form>
     </Pane>
   );
 };
 
 export function RefillStock() {
   const stocks = useSelector(selectAllProducts);
+  const handleRefillStock = () => {
+    console.log("parent wired");
+  };
   return (
     <Pane
       display="grid"
@@ -29,10 +47,16 @@ export function RefillStock() {
       gridColumnGap={8}
       gridRowGap={16}
     >
-      {stocks.map(product => {
+      {stocks.map((product, index) => {
         if (!product.name) return null;
         return (
-          <Stock key={product.name} stock={product.stock} name={product.name} />
+          <Stock
+            key={product.name}
+            index={index}
+            stock={product.stock}
+            name={product.name}
+            sumbitToParent={handleRefillStock}
+          />
         );
       })}
     </Pane>
